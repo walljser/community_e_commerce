46bafb6ea39215d9ec8a903d019075d9c611c3bb
dekvos123 <dekvos123@gmail.com>
2018-05-11 23:01:06 +0800

    update order reducer, order service

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -1,13 +1,18 @@
 import {
   FETCH_ORDERS,
   FETCH_ORDERS_FAILURE,
-  FETCH_ORDERS_SUCCESS
+  FETCH_ORDERS_SUCCESS,
+  POST_ORDER,
+  POST_ORDER_SUCCESS,
+  POST_ORDER_FAILURE,
 } from '../actions/types';
 
 const initialState = {
   isFetching: false,
   errorMessage: '',
-  orders: []
+  orders: [],
+  isPosting: false,
+  postError: ''
 }
 
 export default (state = initialState, action = {}) => {
@@ -27,6 +32,23 @@ export default (state = initialState, action = {}) => {
       return Object.assign({}, state, {
         isFetching: false,
         orders: action.payload
+      })
+    }
+    case POST_ORDER: {
+      return Object.assign({}, state, {
+        isPosting: true,
+        postError: ''
+      })
+    }
+    case POST_ORDER_SUCCESS: {
+      return Object.assign({}, state, {
+        isPosting: false
+      })
+    }
+    case POST_ORDER_FAILURE: {
+      return Object.assign({}, state, {
+        isPosting: false,
+        postError: action.payload
       })
     }
     default:
--- src/services/orderService.js
+++ src/services/orderService.js
@@ -5,12 +5,51 @@ import {
   rest
 } from '../utils';
 
-const getByUserId = async (userId, token) => {
+const DEFAULT_POST = {
+  addressId: 1,
+  remarks: '',
+  // cartDetailIds: []
+}
+
+const getByUserId = async function(userId, token) {
   return await rest.get(userId, token)(
     `${API}/user/${userId}/order`
   )
 }
 
+const create = async function(userId, token, addressId, remarks, cartDetailIds) {
+  cartDetailIds = transArrayIntoString(cartDetailIds)
+  const params = Object.assign({}, DEFAULT_POST, {
+    addressId,
+    remarks,
+    cartDetailIds
+  })
+
+  return await rest.post(userId, token)(
+    `${API}/user/${userId}/order`,
+    params
+  )
+}
+
+function transArrayIntoString(array) {
+  const length = array.length
+  if (length <= 0) {
+    return ''
+  }
+
+  let str = ''
+  for (let i = 0; i < length; i++) {
+    if (i === length - 1) {
+      str += array[i]
+    } else {
+      str += `${array[i]},`
+    }
+  }
+
+  return str
+}
+
 export default {
-  getByUserId
+  getByUserId,
+  create
 }
