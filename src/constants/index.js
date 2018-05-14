1cc732e6225a1e899b67c12cc5e23026ee10c8ca
dekvos123 <dekvos123@gmail.com>
2018-05-15 00:38:25 +0800

    update constants, alter API

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 import areaData from './citys';
 export const areas = areaData.provinces;
-// export const API = 'http://yushijie.club:8080/cloudcommodity/user/v1';
+export const API = 'http://yushijie.club:8080/cloudcommodity/user/v1';
 export const IMAGE_URL = 'http://yushijie.club/cloudimg/goods/';
-export const API = 'http://192.168.191.1:8080/user/v1';
+// export const API = 'http://192.168.191.1:8080/user/v1';
 export const USER_ID = 'userId';
 export const TOKEN = 'token';
 export const PRIMARY_COLOR = '#3f51b5';
